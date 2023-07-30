5a829f08449254a8c8d91d0dc8c946e7d807f82e
milkov97 <kristiyanmilkov@abv.bg>
2023-07-30 12:20:41 +0300

    add push and pop methods

--- DoublyLinkedList/doublyLinkedList.ts
+++ DoublyLinkedList/doublyLinkedList.ts
@@ -24,4 +24,35 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value: T) {
+    // Add element at the end of the linked list
+    const newNode: Node<T> = new Node(value);
+    if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail!.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    // Remove the last element of the linked list
+    if (!this.head) return undefined;
+    const temp: Node<T> = this.tail!;
+    if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+    } else {
+        this.tail = this.tail!.prev;
+        this.tail!.next = null;
+        temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
