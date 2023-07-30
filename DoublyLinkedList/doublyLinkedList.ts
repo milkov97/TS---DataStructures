7fae68039cdb110e2332f623503cf2bc6a972bf0
milkov97 <kristiyanmilkov@abv.bg>
2023-07-30 12:41:24 +0300

    add insert and remove methods

--- DoublyLinkedList/doublyLinkedList.ts
+++ DoublyLinkedList/doublyLinkedList.ts
@@ -29,12 +29,12 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     // Add element at the end of the linked list
     const newNode: Node<T> = new Node(value);
     if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        this.tail!.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
+      this.tail!.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
     this.length++;
     return this;
@@ -45,12 +45,12 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     if (!this.head) return undefined;
     const temp: Node<T> = this.tail!;
     if (this.length === 1) {
-        this.head = null;
-        this.tail = null;
+      this.head = null;
+      this.tail = null;
     } else {
-        this.tail = this.tail!.prev;
-        this.tail!.next = null;
-        temp.prev = null;
+      this.tail = this.tail!.prev;
+      this.tail!.next = null;
+      temp.prev = null;
     }
     this.length--;
     return temp;
@@ -60,15 +60,15 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     // Add element at the begining of the linked list
     const newNode: Node<T> = new Node(value);
     if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        newNode.next = this.head;
-        this.head.prev = newNode;
-        this.head = newNode;
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
     }
     this.length++;
-    return this
+    return this;
   }
 
   shift() {
@@ -76,12 +76,12 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     if (!this.head) return undefined;
     let temp: Node<T> = this.head;
     if (this.length === 1) {
-        this.head = null;
-        this.tail = null;
+      this.head = null;
+      this.tail = null;
     } else {
-        this.head = this.head.next;
-        this.head!.prev = null;
-        temp.next = null;
+      this.head = this.head.next;
+      this.head!.prev = null;
+      temp.next = null;
     }
     this.length--;
     return temp;
@@ -90,18 +90,18 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
   get(index: number) {
     // Return the node at the given index
     if (index < 0 || index > this.length) {
-        return undefined;
+      return undefined;
     }
     let temp: Node<T> = this.head!;
     if (index < this.length / 2) {
-        for (let i = 0; i < index; i++) {
-            temp = temp.prev!;
-        }
+      for (let i = 0; i < index; i++) {
+        temp = temp.prev!;
+      }
     } else {
-        temp = this.tail!;
-        for (let i = this.length - 1; i > index; i++) {
-            temp = temp.prev!;
-        }
+      temp = this.tail!;
+      for (let i = this.length - 1; i > index; i++) {
+        temp = temp.prev!;
+      }
     }
     return temp;
   }
@@ -110,9 +110,42 @@ class DoublyLinkedList<T> implements IDLinkedList<T> {
     // Change the value of the node at the given index
     let temp: Node<T> | undefined = this.get(index);
     if (temp) {
-        temp.value = value;
-        return true;
+      temp.value = value;
+      return true;
     }
     return false;
+  }
+
+  insert(index: number, value: T) {
+    // Insert a new node at the given index
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode: Node<T> = new Node(value);
+    const before: Node<T> = this.get(index)!;
+    const after: Node<T> = before.next!;
+
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index: number) {
+    // Remove element at the given index
+    if (index === 0) return this.shift();
+    if (index === this.length) return this.pop();
+    if (index < 0 || index > this.length) return undefined;
+
+    const temp: Node<T> = this.get(index)!;
+    temp.prev!.next = temp.next;
+    temp.next!.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+    this.length--;
+    return temp;
   }
 }
