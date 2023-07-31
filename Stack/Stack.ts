0f22ca2f0dabc12653ce0b8c71b4bba52be424ad
milkov97 <kristiyanmilkov@abv.bg>
2023-07-31 20:15:13 +0300

    add push method

--- Stack/Stack.ts
+++ Stack/Stack.ts
@@ -3,6 +3,7 @@ import { Node } from "./Node";
 interface Stack<T> {
   top: Node<T> | null;
   length: number;
+  push(value: T): this;
 }
 
 class Stack<T> implements Stack<T> {
@@ -11,6 +12,17 @@ class Stack<T> implements Stack<T> {
     const newNode: Node<T> = new Node(value);
     this.top = newNode;
     this.length = 1;
+  }
+  push(value: T) {
+    const newNode: Node<T> = new Node(value)
+    if (this.length === 0) {
+        this.top = newNode;
+    } else {
+        newNode.next = this.top;
+        this.top = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
 
