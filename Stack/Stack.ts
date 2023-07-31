b9942a71f605510bde3a0a5617788a06927f8fcb
milkov97 <kristiyanmilkov@abv.bg>
2023-07-31 20:23:44 +0300

    add push and pop methods

--- Stack/Stack.ts
+++ Stack/Stack.ts
@@ -4,6 +4,7 @@ interface Stack<T> {
   top: Node<T> | null;
   length: number;
   push(value: T): this;
+  pop(): Node<T> | undefined;
 }
 
 class Stack<T> implements Stack<T> {
@@ -14,17 +15,35 @@ class Stack<T> implements Stack<T> {
     this.length = 1;
   }
   push(value: T) {
-    const newNode: Node<T> = new Node(value)
+    const newNode: Node<T> = new Node(value);
     if (this.length === 0) {
-        this.top = newNode;
+      this.top = newNode;
     } else {
-        newNode.next = this.top;
-        this.top = newNode;
+      newNode.next = this.top;
+      this.top = newNode;
     }
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.top) return undefined;
+    let temp: Node<T> = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 let mS = new Stack(11);
+
+mS.push(12);
+mS.push(13);
 console.log(mS);
+mS.pop()
+mS.pop()
+console.log(mS);
+mS.pop()
+console.log(mS);
+
