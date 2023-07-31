4f394084af91f1bb6a7ffe3d2ba7c772395ecbda
milkov97 <kristiyanmilkov@abv.bg>
2023-07-31 20:52:05 +0300

    add enque and deque methods

--- Queue/Queue.ts
+++ Queue/Queue.ts
@@ -1,23 +1,51 @@
 import { Node } from "./Node";
 
 interface IQueue<T> {
-  first: Node<T>;
-  last: Node<T>;
+  first: Node<T> | null;
+  last: Node<T> | null;
   length: number;
+  enqueue(value: T): this;
+  deque(): Node<T> | undefined;
 }
 
 class Queue<T> implements IQueue<T> {
-    public first: Node<T>;
-    public last: Node<T>
-    public length: number;
-    constructor(value: T) {
-        const newNode: Node<T> = new Node(value)
+  public first: Node<T> | null;
+  public last: Node<T> | null;
+  public length: number;
+  constructor(value: T) {
+    const newNode: Node<T> = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+  enqueue(value: T): this {
+    const newNode: Node<T> = new Node(value);
+    if (!this.first) {
         this.first = newNode;
         this.last = newNode;
-        this.length = 1;
+    } else {
+        this.last!.next = newNode;
+        this.last = newNode;
     }
-
+    this.length++;
+    return this;
+  }
+  deque(): Node<T> | undefined {
+      if (!this.first) return undefined;
+      let temp: Node<T> = this.first;
+      if (this.length === 1) {
+        this.first = null;
+        this.last = null;
+      } else {
+        this.first = this.first.next
+        temp.next = null;
+      }
+      this.length--;
+      return temp;
+  }
 }
 
 let myQueue = new Queue(3);
+myQueue.enqueue(5)
+myQueue.enqueue(6)
 console.log(myQueue);
